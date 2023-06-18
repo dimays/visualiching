f93cf86636d4e9867fa196618ef0ae580d967e11
David Mays <maysidavid@gmail.com>
2023-06-18 17:36:11 -0500

    Added About page, updated Home page with general information about the site, add custom styles

--- visual_i_ching_app/views.py
+++ visual_i_ching_app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from visual_i_ching_app.models import Trigram, Hexagram, HexagramLine, LineType, Reading
 
 def home(request):
-    return render(request, 'visual_i_ching_app/home.html')
+    context = {
+        "hexagrams": Hexagram.objects.all()
+    }
+
+    return render(request, 'visual_i_ching_app/home.html', context=context)
+
+def about(request):
+    context = {
+        "hexagrams": Hexagram.objects.all()
+    }
+
+    return render(request, 'visual_i_ching_app/about.html', context=context)

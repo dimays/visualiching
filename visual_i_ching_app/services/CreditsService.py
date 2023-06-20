3600ef1f202804abd6294b19086809dbfb02c906
David Mays <maysidavid@gmail.com>
2023-06-20 10:35:16 -0500

    Removed broken CreditService code

--- visual_i_ching_app/services/CreditsService.py
+++ visual_i_ching_app/services/CreditsService.py
@@ -8,23 +8,3 @@ import os
 
 
 stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
-
-def create_checkout_session():
-    try:
-        checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': '{{PRICE_ID}}',
-                    'quantity': 1,
-                },
-            ],
-            mode='payment',
-            success_url=YOUR_DOMAIN + '/success.html',
-            cancel_url=YOUR_DOMAIN + '/cancel.html',
-            automatic_tax={'enabled': True},
-        )
-    except Exception as e:
-        return str(e)
-
-    return redirect(checkout_session.url, code=303)

79f29b55e8d6870d72cb5eb33228e5d7e5373dfc
David Mays <maysidavid@gmail.com>
2023-06-29 16:32:39 -0500

    Added html-rendering and removed clicktracking to fix reset password link bug

--- users_app/views.py
+++ users_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.views import PasswordResetView
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.core.mail import EmailMessage
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 
 def register(request):
@@ -15,3 +19,17 @@ def register(request):
         form = UserRegisterForm()
 
     return render(request, 'users_app/register.html', {'form': form})
+
+class CustomPasswordResetView(PasswordResetView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['protocol'] = 'https'
+        context['domain'] = 'www.visualiching.com'
+        print(context)
+        return context
+    
+    def form_valid(self, form):
+        self.request.session['protocol'] = 'https'
+        self.request.session['domain'] = 'www.visualiching.com'
+        
+        return super().form_valid(form)
